7783be4565b8b590928bee05a3149070f30cd088
hengwu <youchuangqiba@163.com>
2018-03-20 14:50:02 +0800

    upd8

--- src/hanlp.py
+++ src/hanlp.py
@@ -99,7 +99,9 @@ if __name__ == '__main__':
     print(summary(document))
 
     # 句法分析
-    view_conllu(list(map(parse_dep, testCases + ["徐先生还具体帮助他确定了把画雄鹰、松鼠和麻雀作为主攻目标。"])))
-    view_conllu(list(map(parse_dep, read_json("./data/source.json"))))
+    text = "徐先生还具体帮助他确定了把画雄鹰、松鼠和麻雀作为主攻目标。"
+    print(parse_dep(text))
+    # view_conllu(list(map(parse_dep, testCases + [text])))
+    # view_conllu(list(map(parse_dep, read_json("./data/source.json"))))
 
     stop_jvm()
